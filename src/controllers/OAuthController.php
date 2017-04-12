b6df58f0a0c989cf3a6c79a560d25c142a3f450a
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-04-12 09:31:48 +0200

    oauth

--- src/controllers/OAuthController.php
+++ src/controllers/OAuthController.php
@@ -5,6 +5,8 @@ namespace BohSchu\Exact\Controllers;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 
 class OAuthController extends BaseController
 {
@@ -17,7 +19,7 @@ class OAuthController extends BaseController
 
     public function login()
     {
-        if (session()->get('access_token') || session()->get('refresh_token')) {
+        if (Cache::get(Auth::id() . '.access_token') || Cache::get(Auth::id() . '.refresh_token')) {
             return redirect()->to('/dashboard');
         }
 
@@ -37,15 +39,22 @@ class OAuthController extends BaseController
     public function callback(Request $request)
     {
         $body = $this->guzzle->request('POST', '/api/oauth2/token', [
-            'code' => $request->get('code'),
-            'redirect_uri' => env('REDIRECT_URI'),
-            'grant_type' => 'authorization_code',
-            'client_id' => env('CLIENT_ID'),
-            'client_secret' => env('CLIENT_SECRET')
+            'headers' => [
+                'Accept' => 'application/json'
+            ],
+            'form_params' => [
+                'code' => $request->get('code'),
+                'redirect_uri' => env('REDIRECT_URI'),
+                'grant_type' => 'authorization_code',
+                'client_id' => env('CLIENT_ID'),
+                'client_secret' => env('CLIENT_SECRET')
+            ]
         ]);
 
-        Cache::forever('access_token', $body['access_token']);
-        Cache::forever('refresh_token', $body['refresh_token']);
+        dd($body);
+
+        Cache::put(Auth::id() . '.access_token', $body['access_token'], 10);
+        Cache::put(Auth::id() . '.refresh_token', $body['refresh_token'], 10);
 
         return redirect()->to('/orders/transfer');
     }
