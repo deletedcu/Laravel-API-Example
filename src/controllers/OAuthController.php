bc34f6d0786a108818acdcde370301ac6ea8e5ea
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-04-12 14:17:50 +0200

    test exact user return

--- src/controllers/OAuthController.php
+++ src/controllers/OAuthController.php
@@ -54,10 +54,10 @@ class OAuthController extends BaseController
 
         $body = json_decode($body->getBody());
 
-        dd($this->authenticateUser($body->access_token));
-
         Cache::put(Auth::id() . '.access_token', $body->access_token, $body->expires_in / 60);
         Cache::forever(Auth::id() . '.refresh_token', $body->refresh_token);
+
+        dd(Cache::get(Auth::id() . '.access_token'));
 
         return redirect()->to('/dashboard');
     }
@@ -73,7 +73,6 @@ class OAuthController extends BaseController
 
         $body = json_decode($body->getBody());
         $user = User::where('email', $body->d->results[0]->Email)->first();
-        dump($user);
 
         return Auth::loginUsingId($user->id);
     }
