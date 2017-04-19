4989df42df230d8b9bc97834233ce377da313137
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-04-19 16:30:22 +0200

    fuck that shit deliver fuck

--- src/ExactHelperTrait.php
+++ src/ExactHelperTrait.php
@@ -78,6 +78,7 @@ trait ExactHelperTrait
             .'/crm/Addresses?$filter=Account eq guid' . "'" . $accountId . "'"
             .' and startswith(trim(AddressLine1),'
             . "'" . $address->delivery_street . "') " . 'eq true'
+            . ' and Type eq 4'
             .' and Postcode eq '. "'" . $address->delivery_zip_code . "'" . '&$select=ID';
 
         $results = $this->get($uri)->d->results;
