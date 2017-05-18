693fa6e18cb1ecbde255fcf739f6bfbdb5181c94
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-05-18 11:37:35 +0200

    test

--- src/ExactHelperTrait.php
+++ src/ExactHelperTrait.php
@@ -120,11 +120,11 @@ trait ExactHelperTrait
         foreach ($products as $key => $value) {
             $uri = '/api/v1/'. $this->division .'/logistics/Items?$filter=trim(Code) eq ' . "'" . $value->variant->sku . "'" . '&$select=ID';
 
-            dd($value->variant->sku);
-
             $itemId = Cache::remember('exact.item.' . $value->variant->sku, 43200, function () use ($uri) {
                 return $this->get($uri)->d->results;
             });
+
+            dd($item);
 
             if(isset($itemId[0]->ID)) {
                 $return[$key]['Item'] = $itemId[0]->ID;
