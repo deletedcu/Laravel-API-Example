8b13519241091b23def45f4d03a49709f96ca9a9
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-05-18 12:14:53 +0200

    fuck u

--- src/ExactHelperTrait.php
+++ src/ExactHelperTrait.php
@@ -119,6 +119,7 @@ trait ExactHelperTrait
 
         foreach ($products as $key => $value) {
             $uri = '/api/v1/'. $this->division .'/logistics/Items?$filter=trim(Code) eq ' . "'" . str_replace('+', '%2B', $value->variant->sku) . "'" . '&$select=ID';
+            dd($this->get($uri)->d->results);
 
             $itemId = Cache::remember('exact.item.' . $value->variant->sku, 43200, function () use ($uri) {
                 return $this->get($uri)->d->results;
