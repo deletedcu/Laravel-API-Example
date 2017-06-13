da88bab3acb5ea69fe788de46bdceff3b90f35db
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-06-13 14:20:03 +0200

    Second draft getSalesOrders

--- src/ExactApi.php
+++ src/ExactApi.php
@@ -44,6 +44,7 @@ class ExactApi
         $uri = '/api/v1/'. $this->division .'/salesorder/SalesOrders'
             . '?$filter=startswith(tolower(YourRef), '. "'e'" .') eq true and substringof('. "'/'" .', YourRef) eq false'
             . '&$expand=SalesOrderLines'
+            . '&$expand=Accounts'
             . '&$select=' . $select;
 
         $response = $this->get($uri);
