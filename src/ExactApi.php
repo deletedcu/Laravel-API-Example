3c575654790318c5d62b480c2258a828ed70cabd
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-06-13 11:44:35 +0200

    First draft getSalesOrders

--- src/ExactApi.php
+++ src/ExactApi.php
@@ -258,7 +258,7 @@ class ExactApi
         $this->checkToken();
 
         $uri = '/api/v1/'. $this->division .'/salesorder/SalesOrders'
-            . '?$filter=startswith(tolower(YourRef), '. "'e'" .') eq true and indexof(YourRef, '. "'/'" .') eq 1'
+            . '?$filter=startswith(tolower(YourRef), '. "'e'" .') eq true and substringof('. "'/'" .', YourRef) eq true'
             . '&$select=OrderID,YourRef';
 
         $response = $this->get($uri);
