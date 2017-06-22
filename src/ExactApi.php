bd59d0f67688c24c103524ecaf139406ed1522cf
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-06-22 11:44:37 +0200

    fix missing invoice contact variable

--- src/ExactApi.php
+++ src/ExactApi.php
@@ -281,6 +281,8 @@ class ExactApi
             'SalesOrderLines' => $salesOrderLines
         ];
 
+        return $data;
+
         $response = $this->post('/api/v1/'. $this->division .'/salesorder/SalesOrders', $data);
 
         if (is_array($response) && array_key_exists('error', $response)) return [$response, null, null, null];
