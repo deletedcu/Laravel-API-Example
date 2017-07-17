3905697e6290c488a043aa40eb701ef04f47ad83
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-07-17 14:53:07 +0200

    test offers

--- src/ExactHelperTrait.php
+++ src/ExactHelperTrait.php
@@ -39,7 +39,6 @@ trait ExactHelperTrait
      */
     protected function getContactId($contact, $accountId)
     {
-        return $contact;
         $uri = '/api/v1/'. $this->division
             .'/crm/Contacts?$filter=Account eq guid' . "'" . $accountId . "'"
             . ' and LastName eq ' . "'" . $contact->last_name . "'"
