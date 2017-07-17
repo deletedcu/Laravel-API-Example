3bfec239c3defbf850562c0b2f0bee8898b78c5a
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-07-17 13:03:17 +0200

    Add getQuotations Method

--- src/ExactApi.php
+++ src/ExactApi.php
@@ -35,9 +35,9 @@ class ExactApi
 
     public function getQuotation()
     {
-        $uri = "/api/v1/{$this->division}/read/crm/Documents" . '?$top=10';
-//        $uri = "/api/v1/{$this->division}/read/crm/DocumentsAttachments"
-//            . '?$filter=ID eq guid' . "'" . '47f297b8-367b-4a88-8876-008a3ab81f61' . "'" . '&$select=AttachmentFileName';
+//        $uri = "/api/v1/{$this->division}/read/crm/Documents" . '?$top=10';
+        $uri = "/api/v1/{$this->division}/read/crm/DocumentsAttachments"
+            . '?$filter=ID eq guid' . "'" . '3efbda32-05f2-414f-a1e1-f9609d4719d8' . "'" . '&$select=AttachmentFileName';
 
         return $this->get($uri);
     }
