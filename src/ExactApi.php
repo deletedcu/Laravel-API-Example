3c9331c0458af72902bdf12827cd50ef576149f2
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-07-20 10:07:11 +0200

    Test docs

--- src/ExactApi.php
+++ src/ExactApi.php
@@ -43,9 +43,9 @@ class ExactApi
 
         $document = $this->get($uri)->d->results[0];
 
-        return $document->Attachments->__deferred->uri;
+        $uri = $document->Attachments->__deferred->uri;
 
-        return $this->get($document->Attachments);
+        return $this->get($uri);
         // $uri = "/api/v1/{$this->division}/read/crm/DocumentsAttachments"
         //     . '?$filter=ID eq guid' . "'" . '3efbda32-05f2-414f-a1e1-f9609d4719d8' . "'" . '&$select=AttachmentFileName';
 
