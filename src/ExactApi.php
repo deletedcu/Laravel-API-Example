3d160d5e8e4a621fc4c3fbf6b885ec95558ca850
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-07-21 11:02:42 +0200

    new setuo

--- src/ExactApi.php
+++ src/ExactApi.php
@@ -42,6 +42,7 @@ class ExactApi
         $quotationUri = "/api/v1/{$this->division}/crm/Quotations"
         . '?filter=QuotationID eq guid' . "'" . $quotationId . "'" . '&select=Document';
 
+        dd($this->get($quotationUri));
         $quotation = $this->get($quotationUri) ? $this->get($quotationUri)->d->results[0] : null;
         dd($quotation);
         // 2. Get the assoc document and its attachment
