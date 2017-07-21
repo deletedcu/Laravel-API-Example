6437fde55f3bb3d16e9dc883c4ffe82d6980ea44
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-07-21 11:04:53 +0200

    new setuo

--- src/ExactApi.php
+++ src/ExactApi.php
@@ -38,9 +38,10 @@ class ExactApi
      */
     public function getQuotation($quotationId)
     {
+        dump($quotationId);
         // 1. Get Quotation by its id
         $quotationUri = "/api/v1/{$this->division}/crm/Quotations"
-        . '?filter=QuotationID eq guid' . "'" . $quotationId . "'" . '&$select=Document';
+        . '?filter=QuotationID eq guid' . "'" . $quotationId . "'" . '&$select=Document,QuotationID,QuotationNumber';
 
         dd($this->get($quotationUri));
         $quotation = $this->get($quotationUri) ? $this->get($quotationUri)->d->results[0] : null;
