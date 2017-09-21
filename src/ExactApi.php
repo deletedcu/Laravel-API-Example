1864cef9730f0a82a5201b5e96fb9379fca81a86
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-09-21 10:26:49 +0200

    test eBill

--- src/ExactApi.php
+++ src/ExactApi.php
@@ -281,6 +281,8 @@ class ExactApi
                 'email' => $order->company->company_email ?? $order->user->email
             ];
 
+            return $eBillData;
+
             $invoiceContact = $this->getContactId($eBillData, $account) ?? $this->createContact($eBillData, $account);
         }
 
