2d91315a5057e1c0ddb8aa2456168c3cc4e01483
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2019-02-26 10:31:36 +0100

    test checkdeliverychanges

--- src/ExactApi.php
+++ src/ExactApi.php
@@ -232,6 +232,7 @@ class ExactApi
         // and check for any address changes
         if ($order->company->erp_id != '') {
             $account = $order->company->erp_id;
+            dd($account);
             $this->checkAddressChanges($account, $order->company, $order->delivery->language->code, $order->digital_bill);
         } else if ($accountId = $this->getAccountId($order->company)) {
             $account = $accountId;
