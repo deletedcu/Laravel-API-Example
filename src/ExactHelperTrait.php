f146496632024518d9217a59a141830d3aa0820c
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2019-02-27 11:37:11 +0100

    test forwarding costs

--- src/ExactHelperTrait.php
+++ src/ExactHelperTrait.php
@@ -182,7 +182,7 @@ trait ExactHelperTrait
     protected function getForwardingCosts($cost)
     {
         $uri = '/api/v1/'.  $this->division .' /logistics/Items?$filter=trim(Code) eq ' . "'" . 1234567 . "'" .'&$select=ID';
-
+        dd($uri);
         $itemId = Cache::remember('exact.item.' . '1234567', 1440, function () use ($uri) {
             return $this->get($uri)->d->results;
         });
